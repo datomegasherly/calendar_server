e96afd9e65f59cf052af5dfcb2031792b938a9ef
datomegasherly <dat.omega.sherly@gmail.com>
2020-11-29 10:19:08 +0330

    add Joi check validation to callCreateFile event

--- event.js
+++ event.js
@@ -1,5 +1,6 @@
 const { EventEmitter } = require('events');
 const fs = require('fs');
+const Joi = require('joi');
 
 class Emitter extends EventEmitter {
     constructor(){
@@ -17,6 +18,27 @@ class Emitter extends EventEmitter {
             });
         });
         this.on('callCreateFile', (res, q) => {
+            const schema = Joi.object({
+                id: Joi.string().min(10).max(40).required(),
+                year: Joi.number().min(1000).max(4000).required(),
+                month: Joi.number().min(1).max(12).required(),
+                day: Joi.number().min(1).max(31).required(),
+                full: Joi.string().min(10).max(10).required(),
+                event: Joi.string().min(3).max(250).required(),
+                start_time: Joi.object({
+                    hour: Joi.number().min(0).max(23).required(),
+                    minute: Joi.number().min(0).max(23).required()
+                }),
+                end_time: Joi.object({
+                    hour: Joi.number().min(0).max(23).required(),
+                    minute: Joi.number().min(0).max(23).required()
+                })
+            });
+            const checkValidate = schema.validate(q);
+            if(checkValidate.error){
+                res.json({error: checkValidate.error.details[0].message});
+                return;
+            }
             fs.readFile('data.json', (err, data) => {
                 let parseData = JSON.parse(data);
                 if(!parseData[q.full]){
