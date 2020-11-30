0be4ff9cec6b89286b7dd0619e58c7c99b81b828
datomegasherly <dat.omega.sherly@gmail.com>
2020-11-30 23:18:52 +0330

    some updates in event class

--- routes/calendar.js
+++ routes/calendar.js
@@ -1,16 +1,23 @@
-const emitter = require('../event');
+const { calendarEmitter } = require('../event');
 const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    emitter.emit('callReadFile', res);
+    calendarEmitter.emit('callReadFile', res);
 });
 
 router.post('/', (req, res) => {
     let data = req.body;
     data.start_time = JSON.parse(data.start_time);
     data.end_time = JSON.parse(data.end_time);
-    emitter.emit('callCreateFile', res, data);
+    calendarEmitter.emit('callCreateFile', res, data);
+});
+
+router.put('/', (req, res) => {
+    let data = req.body;
+    data.start_time = JSON.parse(data.start_time);
+    data.end_time = JSON.parse(data.end_time);
+    calendarEmitter.emit('callEditFile', res, data);
 });
 
 module.exports = router;
