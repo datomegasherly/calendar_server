cd1aff16e2fdd563d92a6c1abfa1cd84297a9cb2
datomegasherly <dat.omega.sherly@gmail.com>
2020-12-02 18:36:26 +0330

    add callDeleteFile event to delete selected event with check of full date

--- event.js
+++ event.js
@@ -1,6 +1,7 @@
 const { EventEmitter } = require('events');
 const fs = require('fs');
 const Joi = require('joi');
+const { parse } = require('path');
 
 class CalendarEmitter extends EventEmitter {
     constructor(){
@@ -79,10 +80,29 @@ class CalendarEmitter extends EventEmitter {
             }
         });
     }
+    delete(res, q){
+        if(q.full && q.id){
+            fs.readFile('data.json', (err, data) => {
+                let parseData = JSON.parse(data);
+                if(!parseData[q.full]) res.json({error: 'This event is not exist'});
+                else {
+                    if(!parseData[q.full].find(r => r.id == q.id)) res.json({error: 'This event is not exist'});
+                    else {
+                        parseData[q.full] = parseData[q.full].filter(r => r.id != q.id);
+                        fs.writeFile('data.json', JSON.stringify(parseData), err => {
+                            if(err) res.json({error: 'Error deleting data'});
+                            else res.json({success: true});
+                        });
+                    }
+                }
+            });
+        } else res.json({error: 'Error delete selected ID'});
+    }
     callEvents(){
         this.on('callReadFile', res => this.read(res));
         this.on('callCreateFile', (res, q) => this.create(res, q));
         this.on('callEditFile', (res, q) => this.write(res, q));
+        this.on('callDeleteFile', (res, q) => this.delete(res, q));
     }
 }
 
