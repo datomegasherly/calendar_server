d94f6f0e53a63d7db8c43bc3a2d025ddf3488db7
datomegasherly <dat.omega.sherly@gmail.com>
2020-12-03 15:35:49 +0330

    use config in router

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const calendar = require('./routes/calendar');
+const config = require('./routes/config');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(bodyParser.json());
 app.use('/api/calendar', calendar);
+app.use('/api/config', config);
 
 app.listen(2020, () => {
     console.log('server is listening on port 2020');
