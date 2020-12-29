af0c0924c18aa9da068501db50f1ef7925d46906
datomegasherly <dat.omega.sherly@gmail.com>
2020-12-29 23:55:15 +0330

    add category data to share with client

--- routes/config.js
+++ routes/config.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     let date = new Date();
-    res.json({date});
+    let category = [{label: 1, value: 'work'}, {label: 2, value: 'exercise'}];
+    res.json({date, category});
 });
 
 module.exports = router;
