1e211e2dbd756cdacd08d70af04817d1f1ab27d3
datomegasherly <dat.omega.sherly@gmail.com>
2020-12-30 12:10:42 +0330

    add status variable to send in config

--- routes/config.js
+++ routes/config.js
@@ -3,8 +3,17 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     let date = new Date();
-    let category = [{label: 1, value: 'work'}, {label: 2, value: 'exercise'}];
-    res.json({date, category});
+    let category = [
+        {label: 1, value: 'work'},
+        {label: 2, value: 'exercise'}
+    ];
+    let status = [
+        {label: 1, value: 'not started'},
+        {label: 2, value: 'on stack'},
+        {label: 4, value: 'in progress'},
+        {label: 3, value: 'completed'}
+    ];
+    res.json({date, category, status});
 });
 
 module.exports = router;
